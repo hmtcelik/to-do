5fcceecb9d3f389a1463550ac09b7ddc7956952d
hmtcelik <abhmtcelik@gmail.com>
2022-04-15 02:16:55 +0300

    set Routers and Links

---
+++ src/Create.js
@@ -0,0 +1,7 @@
+const Create = () => {
+    return (
+        <div className="create"> hello worlds</div>
+    );
+}
+ 
+export default Create;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,16 @@
+import { Link } from 'react-router-dom'; 
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>The Dojo Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create" style={{ 
+        <Link to="/">Home</Link> {/* if we use Link, the links dont want Get request*/}
+        <Link to="/create" style={{
           color: 'white', 
           backgroundColor: '#f1356d',
           borderRadius: '8px' 
-        }}>New Blog</a>
+        }}>New Blog</Link>
       </div>
     </nav>
   );
