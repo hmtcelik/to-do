77e4b84dc6eb49a7615a2bc7a5da64f84bb0a74e
hmtcelik <abhmtcelik@gmail.com>
2022-04-15 01:07:21 +0300

    loading message and logic

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,8 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
   
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -14,12 +11,20 @@ const Home = () => {
   } 
 
   useEffect(() => {
-    console.log('test')
+    fetch('http://localhost:9000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data);
+        setIsPending(false);
+      })
   }, []) // this [] is allow just apply on first reload 
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/> }
     </div>
   );
 }
