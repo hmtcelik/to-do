9c64768225d8ff3c5e7c8f39882fa82ba32610bf
hmtcelik <abhmtcelik@gmail.com>
2022-04-20 02:47:47 +0300

    creating todo on db.json and redirect to home

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,35 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
     const [title, setTitle] = useState(''); //bu method bize dinamik bi sekilde yazilani kontrol etmeyi sagliyor (cok iyi)
     const [body, setBody] = useState('');
     const [auth, setAuth] = useState('User2');
+    const [isPending, setIsPending] = useState(false)
+    const lastPages = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const todo = {title, body, auth};
+        
+        setIsPending(true);
+
+        fetch('http://localhost:9000/blogs/', {
+            method: 'POST',
+            headers: { 'Content-Type':'application/json'},
+            body: JSON.stringify(todo)
+        }).then(() => {
+            setIsPending(false);
+            //lastPages.go(-1); //going last page (-1)
+            lastPages.push('/') //redirect to '/'
+        });
+
+    }
 
     return (
         <div className="create"> 
             <h2>Create a New Todo</h2>
-            <form>
+            <form on onSubmit={handleSubmit}>
                 <label>Title:</label>
                 <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
             
@@ -21,13 +42,10 @@ const Create = () => {
                     <option value="User2">User2</option>
                 </select>
 
-                <button>Create</button>
+                { !isPending && <button>Create</button> }
+                { isPending && <button>Creating...</button> }
             </form>
-            <h3>{title}</h3>
-            <p>{body}</p>
-            <i>{ auth }</i>
         </div>
-        
     );
 }
  
